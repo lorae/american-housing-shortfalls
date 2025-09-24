99bb4ed04894227cf135d4e701e085dcd7e606f9
Lorae <10428186+lorae@users.noreply.github.com>
2025-09-24 15:32:23 -0400

    feat: generate data extract

--- src/scripts/get-ipums-data.R
+++ src/scripts/get-ipums-data.R
@@ -3,17 +3,52 @@
 # The purpose of this script is to download the IPUMS data extract directly from
 # the IPUMS API.
 
+# ----- Step 0: Configuration ----- #
+
 library(ipumsr)
 
 if (!file.exists(".Renviron")) {
   stop(".Renviron file needed for this code to run. Please refer to Part B of the README file for configuration instructions.")
 } 
 
-# Read API key from environment (set in .Renviron)
+# Read API key from project-local .Renviron
 readRenviron(".Renviron") # Force a re-read each run
 api_key <- Sys.getenv("IPUMS_API_KEY")
 
-if (api_key == "your_ipums_api_key") {
+if (api_key == "" || api_key == "your_ipums_api_key") {
   stop(".Renviron file exists, but IPUMS API key has not been added. Please refer to Part B of the README file for configuration instructions.")
 }
 
+print(paste0("IPUMS API key: ", api_key))
+set_ipums_api_key(api_key)
+
+# ----- Step 1: Define, submit, and wait for data extract ----- #
+
+# Define extract
+ipums_extract <- define_extract_micro(
+  description = "Replication data: Changes in Average Household Size and Headship Rates as Indicators of Housing Shortfalls (Hepburn and Stojanovic, 2025)",
+  collection = "usa",
+  samples = c(
+    "us2000a", # 2000 5% sample
+    "us2019c"  # 2015-2019, ACS 5-year sample
+    ),
+  variables = c(
+    # Household-level
+    "YEAR", "MULTYEAR", "SAMPLE", "SERIAL", "CBSERIAL", "HHWT",
+    "CLUSTER", "STRATA", "GQ", "NUMPREC", "CPUMA0010",
+    "OWNERSHP", "OWNERSHPD", "HHINCOME", "ROOMS", 
+    "BEDROOMS",
+    # Person-level
+    "PERNUM", "PERWT","SEX", "AGE",
+    "RACE", "RACED", "HISPAN", "HISPAND",
+    "BPL", "BPLD", "CITIZEN",
+    "EDUC", "EDUCD", "INCTOT"
+    # "REPWTP", "UNITSSTR", # Probably not needed, add back in if needed
+  )
+)
+
+# Submit extract request
+submitted <- submit_extract(ipums_extract)
+
+# Poll every 10 seconds until extract is ready
+wait_for_extract(submitted) 

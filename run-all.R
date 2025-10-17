fa520ced47af1af52c8a94eac9a3be2e7d12b6f1
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2025-10-17 14:53:55 -0400

    feat: add to control script

--- run-all.R
+++ run-all.R
@@ -2,7 +2,18 @@
 # 
 # Runs full replication code for the entire project
 
+# Read in raw data, process to clean data
 source("src/scripts/import-ipums.R")
 source("src/scripts/process-ipums.R")
 
+
+# Figures
+source("src/figures/accessory-fig06-hhsize-age-2per-line.R")
+# TODO: other figures here
+
+# McClure-Schwartz (YYYY) replication
+# TODO: replace placeholder with actual year
 source("src/scripts/mcclure-schwartz-replication-state-surplus.R")
+
+# Fast facts
+source("src/scripts/fast-facts-v2.R")

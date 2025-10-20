c6393ff932ee3f0d34997da8667dd4e049d7e2c2
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2025-10-20 15:51:30 -0400

    feat: add final counterfactual calculation

--- src/scripts/fast-facts-v2.R
+++ src/scripts/fast-facts-v2.R
@@ -387,216 +387,63 @@ headship_cf_cpuma |>
   pull(surfeit) |>
   sum()
 
-# ------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------
+# FAST FACT: Additional households to match white household sizes
+# Page 11
+# "Considerably more households would be necessary to allow for further declines in 
+# average household size. For example, for the full U.S. population in 2019 to experience 
+# patterns on par with what is typical for white Americans, the nation would need 
+# between 8.358 million additional households (minimum based on average household 
+# size) and 13.032 million additional households (based on headship rates)
+#-------------------------------------------------------------------------------
 
-ipums_db <- tbl(con, "ipums_processed")
-
-# Calculate CPUMA-level fully-controlled diffs
-hhsize_contributions <- calculate_counterfactual(
-  cf_categories = c("RACE_ETH_bucket", "AGE_bucket", "gender", "us_born", "EDUC_bucket", "INCTOT_cpiu_2010_bucket", "CPUMA0010", "tenure"),
-  p0 = 2000,
-  p1 = 2019,
-  p0_data = ipums_db |> filter(YEAR == 2000, GQ %in% c(0,1,2)), 
-  p1_data = ipums_db |> filter(YEAR == 2019, GQ %in% c(0,1,2)),
-  outcome = "NUMPREC"
-)$contributions  
-
-# Counterfactual overall household size in 2019
-cf_hhsize_2019_overall <- (hhsize_contributions$weighted_mean_2000 * hhsize_contributions$percent_2019) |> sum() / 100
-cf_hhsize_2019_overall
-
-population_aggregates_2019 <- crosstab_mean(
-  data = ipums_db |> filter(YEAR == 2019, GQ %in% c(0,1,2)),
+# Pull the average number of people in a white person's home in 2019
+white_hhsize_2019 <- crosstab_mean(
+  data = ipums_db |> filter(GQ %in% c(0,1,2)),
   value = "NUMPREC",
   wt_col = "PERWT",
-  group_by = c(),
-  every_combo = TRUE)
-
-# Actual overall household size in 2019 (excluding those in Group Quarters)
-act_hhsize_2019_overall <- population_aggregates_2019 |>
+  group_by = c("RACE_ETH_bucket", "YEAR")
+) |>
+  filter(RACE_ETH_bucket == "White", YEAR == 2019) |>
   pull(weighted_mean)
-act_hhsize_2019_overall
 
-# Population in 2019 (excluding those in Group Quarters)
-population_2019 <- population_aggregates_2019 |>
-  pull(weighted_count)
-population_2019
-
-## Overall housing unit shortage/surfeit in 2019 (netting out positives and negatives within CPUMAs)
-(population_2019/act_hhsize_2019_overall) - (population_2019/cf_hhsize_2019_overall)
-
-##############################################
-# Fast fact: number of additional housing units needed, accounting for CPUMA-level surfeit/surplus
-##############################################
-
-# Sum the counterfactuals by CPUMA
-hhsize_contributions_cpuma <- hhsize_contributions |>
+# Use this value on all CPUMAs
+hhsize_cf_cpuma |>
   mutate(
-    contribution = weighted_mean_2000 * percent_2019 / 100
+    white_hhsize_2019 = white_hhsize_2019,
+    white_diff = white_hhsize_2019 - observed_2000
+    ) |>
+  filter(white_diff < 0) |>
+  mutate(
+    p_over_s_2019 = pop_2019 / observed_2019,
+    p_over_s_white_cf = pop_2019 / white_hhsize_2019,
+    minimum_surfeit = p_over_s_white_cf - p_over_s_2019
   ) |>
-  group_by(CPUMA0010) |>
-  summarize(
-    contribution = sum(contribution, na.rm = TRUE),
-    population = sum(weighted_count_2019, na.rm = TRUE),
-    percent = sum(percent_2019, na.rm = TRUE),
-    .groups = "drop"
-  ) |>
-  mutate(cf_hhsize = contribution/percent * 100)
-
-# Validity check: ensure the sum of contributions equals cf_hhsize_2019_overall
-# Rounded to 5 decimal places due to floating point errors
-round((hhsize_contributions_cpuma$contribution |> sum()), 5) == round(cf_hhsize_2019_overall, 5)
-
-# Validity check: sum of `population` column matches population_2019
-sum(hhsize_contributions_cpuma$population) == population_2019
-
-# Validity check: cf_hhsize*percent/100 |> sum() = cf_hhsize_2019_overall
-round(((hhsize_contributions_cpuma$cf_hhsize*hhsize_contributions_cpuma$percent/100)) |> sum(), 5) == round(cf_hhsize_2019_overall, 5)
-
-cf_by_cpuma <- cpuma_hhsize |>
-  rename(CPUMA0010 = subgroup) |>
-  left_join(hhsize_contributions_cpuma, by = "CPUMA0010") |>
-  mutate(
-    housing_surfeit = (population/hhsize_2019) - (population/cf_hhsize)
-  )
-
-# Housing shortage and surplus, and number of CPUMAs with a shortage or surplus
-cf_by_cpuma_summary <- cf_by_cpuma |> 
-  summarize(
-    total = sum(housing_surfeit, na.rm = TRUE),
-    count_total = sum(!is.na(housing_surfeit)),
-    sum_negative = sum(housing_surfeit[housing_surfeit < 0], na.rm = TRUE),
-    sum_positive = sum(housing_surfeit[housing_surfeit > 0], na.rm = TRUE),
-    count_negative = sum(housing_surfeit < 0, na.rm = TRUE),
-    count_positive = sum(housing_surfeit > 0, na.rm = TRUE)
-  )
-cf_by_cpuma_summary
-
-# Percentage with a shortage
-pull(cf_by_cpuma_summary, count_negative) / pull(cf_by_cpuma_summary, count_total)
-
-# Largest surfeit
-slice_max(cf_by_cpuma, housing_surfeit) |> View()
-# Largest shortage
-slice_min(cf_by_cpuma, housing_surfeit) |> View()
-
-# Figure out where the greatest surfeit/surplus is by mapping.
-# TODO: Don't just copy and paste this code from counterfactual-regional.R. Create
-# a function that can be called to map things.
-rot <- function(a) {
-  matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
-}
-transform_state <- function(
-    df, 
-    state_fp, 
-    rotation_angle, 
-    scale_factor, 
-    shift_coords
-) {
-  state <- df %>% filter(STATEFIP == state_fp)
-  state_geom <- st_geometry(state)
-  state_centroid <- st_centroid(st_union(state_geom))
-  rotated_geom <- (state_geom - state_centroid) * rot(rotation_angle * pi / 180) / scale_factor + state_centroid + shift_coords
-  state %>% st_set_geometry(rotated_geom) %>% st_set_crs(st_crs(df))
-}
-# Load shapefiles. Data is unzipped from WHERE? TODO: document
-cpuma_sf <- st_read("data/ipums-cpuma0010-sf/ipums_cpuma0010.shp") |>
-  filter(!STATEFIP %in% c('60', '64', '66', '68', '69', '70', '72', '78')) |># Remove excluded states, like Puerto Rico
-  st_transform(crs = "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs") |>
-  mutate(geometry = st_simplify(geometry, dTolerance =  5000))  # Simplify shapes
-
-# Rotate and move Alaska and Hawaii to fit on map
-alaska_cpuma <- transform_state(cpuma_sf, "02", -39, 2.3, c(1000000, -5000000))
-hawaii_cpuma <- transform_state(cpuma_sf, "15", -35, 1, c(5200000, -1400000))
-
-# Final map after transforming non-contiguous states
-cpuma_sf_final <- cpuma_sf |>
-  filter(!STATEFIP %in% c("02", "15")) |>
-  bind_rows(alaska_cpuma, hawaii_cpuma)
-
-# Highlight CPUMA 973 in red
-cpuma_sf_hhsize <- cpuma_sf_final |>
-  mutate(
-    fill_color = ifelse(CPUMA0010 == "973", "red", "white")
-  )
-ggplot(cpuma_sf_hhsize) + 
-  geom_sf(aes(geometry = geometry, fill = fill_color), color = "grey50", size = 0.1) +
-  scale_fill_identity() + 
-  theme_void()
+  pull(minimum_surfeit) |>
+  sum()
 
 
-# Highlight CPUMA 550 in red
-cpuma_sf_hhsize <- cpuma_sf_final |>
-  mutate(
-    fill_color = ifelse(CPUMA0010 == "550", "red", "white")
-  )
-ggplot(cpuma_sf_hhsize) + 
-  geom_sf(aes(geometry = geometry, fill = fill_color), color = "grey50", size = 0.1) +
-  scale_fill_identity() + 
-  theme_void()
-
-
-##############################################
-# Fast fact: number of additional housing units needed, all CPUMAs have average HH size of white americans
-##############################################
-
-# Average size of a white household
-white_hhsize_2019_overall <- crosstab_mean(
-  data = ipums_db |> filter(YEAR == 2019) |> filter(GQ %in% c(0,1,2)),
-  value = "NUMPREC",
+# Pull the average headship rate among white people in 2019
+white_headship_2019 <- crosstab_mean(
+  data = ipums_db |> filter(GQ %in% c(0,1,2)),
+  value = "is_hoh",
   wt_col = "PERWT",
-  group_by = c("RACE_ETH_bucket")
-) |> 
-  filter(RACE_ETH_bucket == "White") |>
+  group_by = c("RACE_ETH_bucket", "YEAR")
+) |>
+  filter(RACE_ETH_bucket == "White", YEAR == 2019) |>
   pull(weighted_mean)
 
-## Overall housing unit shortage/surfeit in 2019 relative to white household norms (netting out positives and negatives within CPUMAs)
-(population_2019/act_hhsize_2019_overall) - (population_2019/white_hhsize_2019_overall) # huge number!
-
-## cPUMA-level household counterfactual, except we replace every cPUMA's cf_hhsize with 3.09 (the value of `white_hhsize_2019_overall`)
-hhsize_contributions_cpuma_white <- hhsize_contributions_cpuma |>
-  select(-cf_hhsize) |>
-  mutate(cf_hhsize_white = white_hhsize_2019_overall)
-
-cf_by_cpuma_white <- cpuma_hhsize |>
-  rename(CPUMA0010 = subgroup) |>
-  left_join(hhsize_contributions_cpuma_white, by = "CPUMA0010") |>
+# Use this value on all CPUMAs
+headship_cf_cpuma |>
   mutate(
-    housing_surfeit = (population/hhsize_2019) - (population/cf_hhsize_white)
-  )
+    white_headship_2019 = white_headship_2019,
+    white_diff = white_headship_2019 - observed_2000
+    ) |>
+  filter(white_diff > 0) |>
+  mutate(
+    surfeit = white_diff * pop_2019
+  ) |>
+  pull(surfeit) |>
+  sum()
 
-# Housing shortage and surplus, and number of CPUMAs with a shortage or surplus
-cf_by_cpuma_summary_white <- cf_by_cpuma_white |> 
-  summarize(
-    total = sum(housing_surfeit, na.rm = TRUE),
-    count_total = sum(!is.na(housing_surfeit)),
-    sum_negative = sum(housing_surfeit[housing_surfeit < 0], na.rm = TRUE),
-    sum_positive = sum(housing_surfeit[housing_surfeit > 0], na.rm = TRUE),
-    count_negative = sum(housing_surfeit < 0, na.rm = TRUE),
-    count_positive = sum(housing_surfeit > 0, na.rm = TRUE)
-  )
-cf_by_cpuma_summary_white
-
-# Percentage with a shortage
-pull(cf_by_cpuma_summary_white, count_negative) / pull(cf_by_cpuma_summary_white, count_total)
-
-######### Fast fact: nultiracial percentage
-# We define “multiracial” individuals as non-Hispanic persons whose race is encoded 
-# as “Two major races” or “Three or more major races” in the Census Bureau’s “RACE” 
-# variable. These individuals comprised __% of the non group-quartered population 
-# in 2000 and ___% in 2019.
-crosstab_percent(
-  data = ipums_db |> filter(GQ %in% c(0,1,2)), 
-  wt_col = "PERWT", 
-  group_by = c("RACE_ETH_bucket", "YEAR"),
-  percent_group_by = c("YEAR")
-) |> arrange(YEAR)
-
-##### Fast fact: median CPUMA-level hhsize in 2000
-tabulate_summary(
-  ipums_db,
-  year = 2000,
-  value = "NUMPREC",
-  group_by = "CPUMA0010"
-) |> pull(hhsize) |> median()
